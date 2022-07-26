5834198fc166755904490749f0272697bafb69b0
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-07-25 23:06:11 -0500

    homepage is done

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -7,6 +10,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
